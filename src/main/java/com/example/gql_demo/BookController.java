686c893a2583fa16c8984f73576b23b2ec1c58db
Michael Seemann <michael.seemann@synaos.com>
2024-08-31 11:32:37 +0200

    with subscriptions

--- src/main/java/com/example/gql_demo/BookController.java
+++ src/main/java/com/example/gql_demo/BookController.java
@@ -3,10 +3,14 @@ package com.example.gql_demo;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SubscriptionMapping;
 import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 @Controller
@@ -22,6 +26,12 @@ public class BookController {
         return Book.byFilter(filter);
     }
 
+    @SubscriptionMapping
+    public Flux<Book> bookSubscription() {
+        Random random = new Random();
+        return Flux.interval(Duration.ofSeconds(1))
+            .map(num -> Book.getById("book-" + (random.nextInt(4) + 1)));
+    }
 //    @SchemaMapping
 //    public Author author(Book book) {
 //        return Author.getById(book.authorId());
