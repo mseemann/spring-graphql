1c8e88cf13444c9e2a30365d408e7d96e43d4401
Michael Seemann <michael.seemann@synaos.com>
2024-08-31 09:57:00 +0200

    with batch mapping

--- src/main/java/com/example/gql_demo/BookController.java
+++ src/main/java/com/example/gql_demo/BookController.java
@@ -1,11 +1,13 @@
 package com.example.gql_demo;
 
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.BatchMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class BookController {
@@ -20,8 +22,18 @@ public class BookController {
         return Book.byFilter(filter);
     }
 
-    @SchemaMapping
-    public Author author(Book book) {
-        return Author.getById(book.authorId());
+//    @SchemaMapping
+//    public Author author(Book book) {
+//        return Author.getById(book.authorId());
+//    }
+
+    @BatchMapping
+    Map<Book, Author> author(List<Book> books) {
+        System.out.println(books);
+        return books.stream()
+            .collect(Collectors.toMap(
+                book -> book,
+                book -> Author.getById(book.authorId())
+            ));
     }
 }
